a07e0c672ab26861e747d6b9a868cada389624ed
Dc-Valerian <valerianpedro03@gmail.com>
2025-10-31 10:28:41 +0100

    Added

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 class="text-3xl font-bold underline text-[red]">Hello world!</h1>
+      <h3 class="text-3xl font-bold underline text-[yellow]">Hello world!</h3>
+    </>
+  );
+}
+
+export default App;
